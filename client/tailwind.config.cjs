5ffb6f8153e572cec83e65bad207f7a3718c315b
Rajnish Kumar <rajnisharyankumar@gmail.com>
2023-04-20 04:01:33 +0530

    Update Home Pages

--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", "./node_modules/tw-elements/dist/js/**/*.js",],
   theme: {
     extend: {
       fontFamily: {
@@ -20,5 +20,5 @@ module.exports = {
       },
     },
   },
-  plugins: [require("@tailwindcss/forms")],
+  plugins: [require("@tailwindcss/forms"),require("tw-elements/dist/plugin")],
 };
